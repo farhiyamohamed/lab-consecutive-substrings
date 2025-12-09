ba8b9e35c3dedb5a678d389825dde8ed2b4d51a3
farhiyamohamed <farhiyam518@gmail.com>
2025-12-09 12:16:34 +0300

    Completed consecutive substrings lab

--- consecutive_substrings.js
+++ consecutive_substrings.js
@@ -1,18 +1,23 @@
-function consecutiveSubstrings(string) {
-  // type your code here
+/**
+ * getConsecutiveSubstrings(str)
+ * Returns an array of all consecutive substrings (length >= 1)
+ * in the order they appear.
+ *
+ * Example: 'abc' => ['a','ab','abc','b','bc','c']
+ */
+
+function getConsecutiveSubstrings(str) {
+  const result = [];
+
+  // Loop through starting positions
+  for (let i = 0; i < str.length; i++) {
+    // Loop through ending positions
+    for (let j = i + 1; j <= str.length; j++) {
+      result.push(str.slice(i, j));
+    }
+  }
+
+  return result;
 }
 
-if (require.main === module) {
-  // add your own tests in here
-  console.log("Expecting: ['a', 'ab', 'abc', 'b', 'bc', 'c']");
-  console.log("=>", consecutiveSubstrings('abc'));
-
-  console.log("");
-
-  console.log("Expecting: ['a']");
-  console.log("=>", consecutiveSubstrings('a'));
-}
-
-module.exports = consecutiveSubstrings;
-
-
+module.exports = getConsecutiveSubstrings;
